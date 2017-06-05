cbd0a0b2486b4d0c31c97eb29f8d75810fba7980
claysissing <claysissing@gmail.com>
2017-06-05 11:50:12 +0100

    updated to web component v1 and did some cleaning when component is remove from dom

--- dist/floating-action.js
+++ dist/floating-action.js
@@ -1,116 +1,119 @@
+/* globals HTMLElement, customElements */
 /**
  * Created by claysissing on 12/07/2016.
  */
 
-var csFA = Object.create(HTMLElement.prototype);
+class Csfloatingaction extends HTMLElement {
+  connectedCallback () {
+    this.setAttributes()
+    this.setElements()
+    this.addEvents()
+    this.hasScrolled()
+  }
 
-csFA.createdCallback = function() {
-    this.setAttributes();
-    this.setElements();
-    this.addEvents();
-    this.hasScrolled();
-};
+  disconnectedCallback () {
+    this.removeEventListener('mouseenter', this.buttonAction)
+    this.removeEventListener('mouseleave', this.buttonAction)
+    this.removeEventListener('click', this.buttonAction)
+    window.removeEventListener('scroll', this.scrollEvent)
+    this.buttonElement.removeEventListener('click', this.preventBtnClick)
+  }
 
-csFA.setAttributes = function() {
-    this.button = this.getAttribute('button');
-    this.container = this.getAttribute('container');
-    this.toggle = this.getAttribute('toggle');
-    this.moveContainer = this.getAttribute('move-container');
-    this.offSet = parseInt(this.getAttribute('to-bottom')) || 0;
-};
+  setAttributes () {
+    this.button = this.getAttribute('button')
+    this.container = this.getAttribute('container')
+    this.toggle = this.getAttribute('toggle')
+    this.moveContainer = this.getAttribute('move-container')
+    this.offSet = parseInt(this.getAttribute('to-bottom')) || 0
+  }
 
-csFA.setElements = function() {
-    this.buttonElement = document.querySelector(this.button);
-    this.containerElement = document.querySelector(this.container);
-};
+  setElements () {
+    this.buttonElement = this.querySelector(this.button)
+    this.containerElement = this.querySelector(this.container)
+  }
 
-csFA.addEvents = function() {
-    
-    if(!this.isTouchDevice()) {
-        this.addEventListener('mouseenter', this.buttonAction.bind(this), false);
-        this.addEventListener('mouseleave', this.buttonAction.bind(this), false);
+  addEvents () {
+    if (!this.isTouchDevice()) {
+      this.addEventListener('mouseenter', this.buttonAction.bind(this), false)
+      this.addEventListener('mouseleave', this.buttonAction.bind(this), false)
     }
-    
-    if(this.isTouchDevice()) {
-        this.buttonElement.addEventListener('click', this.buttonAction.bind(this), false);
+
+    if (this.isTouchDevice()) {
+      this.buttonElement.addEventListener('click', this.buttonAction.bind(this), false)
     }
-};
+  }
 
-csFA.hasScrolled = function() {
-    
-    window.addEventListener('scroll', function() {
-        
-        var isBottom = this.isBottom(this.offSet),
-            hasClass = this.classList.contains(this.moveContainer);
-        
-        if(isBottom) {
-            if(!hasClass) {
-                this.classList.add(this.moveContainer);
-            }
-        }
-        
-        if(!isBottom) {
-            if(hasClass) {
-                this.classList.remove(this.moveContainer);
-            }
-        }
-        
-    }.bind(this), false);
-};
+  hasScrolled () {
+    window.addEventListener('scroll', this.scrollEvent.bind(this), false)
+  }
 
-csFA.buttonAction = function(e) {
-    
-    e.preventDefault();
-    this.addRemoveClass();
-    
-    this.buttonElement.addEventListener('click', function(e) {
-        e.preventDefault();
-    }, false);
-};
+  scrollEvent () {
+    var isBottom = this.isBottom(this.offSet)
+    var hasClass = this.classList.contains(this.moveContainer)
 
-csFA.addRemoveClass = function () {
-    
-    var conElement = this.containerElement,
-        isVisible = conElement.classList.contains(this.toggle);
-    
-    if(!isVisible) {
-        conElement.classList.add(this.toggle);
+    if (isBottom) {
+      if (!hasClass) {
+        this.classList.add(this.moveContainer)
+      }
     }
-    
-    if(isVisible) {
-        conElement.classList.remove(this.toggle);
-    }
-};
 
-csFA.isTouchDevice = function() {
+    if (!isBottom) {
+      if (hasClass) {
+        this.classList.remove(this.moveContainer)
+      }
+    }
+  }
+
+  buttonAction (e) {
+    e.preventDefault()
+    this.addRemoveClass()
+    this.buttonElement.addEventListener('click', this.preventBtnClick, false)
+  }
+
+  preventBtnClick (e) {
+    e.preventDefault()
+  }
+
+  addRemoveClass () {
+    var conElement = this.containerElement
+    var isVisible = conElement.classList.contains(this.toggle)
+    if (!isVisible) {
+      conElement.classList.add(this.toggle)
+    }
+    if (isVisible) {
+      conElement.classList.remove(this.toggle)
+    }
+  }
+
+  isTouchDevice () {
     try {
-        document.createEvent('TouchEvent');
-        return true;
+      document.createEvent('TouchEvent')
+      return true
     } catch (e) {
-        return false;
+      return false
     }
-};
+  }
 
-csFA.isBottom = function(offSet) {
-    
-    var totalHeight   = document.body.offsetHeight - offSet,
-        visibleHeight = document.documentElement.clientHeight,
-        currentScroll;
-    
+  isBottom (offSet) {
+    var totalHeight = document.body.offsetHeight - offSet
+    var visibleHeight = document.documentElement.clientHeight
+    var currentScroll
+
     if (document.documentElement.scrollTop) {
-        currentScroll = document.documentElement.scrollTop;
+      currentScroll = document.documentElement.scrollTop
     } else {
-        currentScroll = document.body.scrollTop;
+      currentScroll = document.body.scrollTop
     }
-    
+
     if (totalHeight <= currentScroll + visibleHeight) {
-        return true;
+      return true
     } else {
-        return false;
+      return false
     }
+  }
+
+
 }
 
-
-document.registerElement('cs-floatingaction', {
-    prototype: csFA
-});
+// Define the new element
+customElements.define('cs-floatingaction', Csfloatingaction)
